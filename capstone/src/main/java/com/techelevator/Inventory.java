1af05692d869fcf78a9fe9ce98a40acbedea8b2a
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-06-09 21:45:09 -0400

    update

---
+++ capstone/src/main/java/com/techelevator/Inventory.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Inventory {
+
+    private Scanner inventoryMenu;
+    String inventoryFile = "vendingmachine.csv";
+    File inventoryList = new File(inventoryFile);
+   private final Scanner inventoryMenu = new Scanner(inventoryList);
+
+           
+
+
+
+}
--- capstone/src/main/java/com/techelevator/Items.java
+++ capstone/src/main/java/com/techelevator/Items.java
@@ -6,7 +6,16 @@ public class Items {
     private double price;
     private String brandName;
     private String slotLocation;
+    private int quantity = 5;
 
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     public double getPrice() {
         return price;
