14ed0d91c3897cf5ac2ea9718218c1897636e0d6
Delayne Stewart Jr <dstewartjr1@gmail.com>
2023-06-09 23:35:59 -0400

    vending machince class updates

--- capstone/src/main/java/com/techelevator/Inventory.java
+++ capstone/src/main/java/com/techelevator/Inventory.java
@@ -1,17 +1,24 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Inventory {
 
-    private Scanner inventoryMenu;
+
     String inventoryFile = "vendingmachine.csv";
     File inventoryList = new File(inventoryFile);
-   private final Scanner inventoryMenu = new Scanner(inventoryList);
 
-           
+    public void scanInventory() {
+        try (Scanner inventoryMenu = new Scanner(inventoryList)) {
+            while (inventoryMenu.hasNextLine()) {
+                String list = inventoryMenu.nextLine();
+            }
+            } catch(FileNotFoundException e){
+                System.out.println("invalid file");
 
 
-
+            }
+    }
 }
